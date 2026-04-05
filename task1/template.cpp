baa7b32c976514b24669a2f058c13114bf95c94f
saiiiiiiiiii <sai.nadre28@gmail.com>
2026-04-05 16:34:56 +0530

    commit

--- task1/template.cpp
+++ task1/template.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
+ #include <opencv2/opencv.hpp>
 
 using namespace std;
 
@@ -17,12 +17,27 @@ int main()
          2,  0, -2,
          1,  0, -1
     );
-
+    
+    
+    
     cv::Mat img = cv::imread("./assets/hogwarts.png", cv::IMREAD_GRAYSCALE);
     if (img.empty()) {
         cerr << "Could not load image at ./assets/hogwarts.png\n";
         return -1;
     }
+
+    cv::Mat output;
+    cv::namedWindow("filter 2D output", cv::WINDOW_NORMAL);
+
+
+    // using the built-in function
+    cv::filter2D(img, output, -1, K1*K2, cv::Point(-1, -1), 5.0, cv::BorderTypes::BORDER_REPLICATE);
+    
+    cv::imshow("filter 2D output", output);
+    cv::waitKey(0);
+    
+    cout <<"9 multiplications per pixel is required by our approach"<<endl;
+    
 
     cv::Mat img_f;
     img.convertTo(img_f, CV_64FC1);
